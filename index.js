24193d0c0003dd2080294165a0ab86937764c309
Douglas Hellberg <DougHellberg@gmail.com>
2023-08-01 15:18:44 -0400

    Lab complete

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 // Write your solution in this file!
 const employee = {
-   name: "Doug",
-   street: "12 Cake Ln",
-};
+    name: "Doug",
+    address: "123 Doug St"
+}
+
 
 function updateEmployeeWithKeyAndValue(employee, key, value){
-    const newEmployee={...employee};
-    newEmployee[key] = value;
-    return newEmployee;
- }
+    const newEmployee = {...employee}
 
- function destructivelyUpdateEmployeeWithKeyAndValue(employee,key, value){
-    employee[key] = value;
-    return employee;
- }
-function deleteFromEmployeeByKey(employee,key){
-    const newEmployee={...employee};
-    delete newEmployee[key];
+    newEmployee[key] = value
     return newEmployee;
 }
-function destructivelyDeleteFromEmployeeByKey(employee,key){
-    delete employee[key];
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value
     return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee}
+    
+    delete newEmployee[key]
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
 }
